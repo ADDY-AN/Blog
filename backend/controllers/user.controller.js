afaa4aabb9213c35ad475ede87911a4135eba9ee
Aditya Anand <106367942+ADDY-AN@users.noreply.github.com>
2025-05-27 20:02:49 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -96,8 +96,8 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV === "production"
+            sameSite: "None",
+            secure: true,
         }).json({
             success: true,
             message: `Welcome back ${userData.firstName}`,
